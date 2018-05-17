d83fa4e18cf411a1e4a68f16eb48f1e3d09dbe73
Unknown <luca.macedone@outlook.it>
2018-05-17 18:36:17 +0200

    LAB1: to test

--- Labo1/ASD-doubly-linked-list.cpp
+++ Labo1/ASD-doubly-linked-list.cpp
@@ -12,7 +12,7 @@ node *next;
 };
 
 /*************************************************/
-/* Implementazione delle operazioni di utilita'  */
+/* Implementazione delle operazioni di utilita'ï¿½ */
 /*************************************************/
 
 void readFromStream(istream& str, List& l)
@@ -60,65 +60,166 @@ void print(const List& l)           /* stampa la lista */
 
 void list::clear(const List& l)                 /* "smantella" la lista svuotandola */
 {
-
+    node* cur = l->next;
+    node* aux;
+    while(cur != l){
+        aux = cur;
+        cur = cur->next;
+        delete aux;
+    }
+    l->next = l;
+    l->prev = l;
 }
 
 
 Elem list::get(int pos, const List& l)        /* restituisce l'elemento in posizione pos se presente; restituisce un elemento che per convenzione si decide che rappresenta l'elemento vuoto altrimenti*/
 {
-                        
+    if(pos > size(l)){
+        return EMPTYELEM;
+    }else{
+        int n = 0;
+        node* cur = l->next;
+        while((cur != l) && (n != pos)){
+            n++;
+            cur = cur->next;
+        }
+        return cur->info;
+    }
 }
 
 
 void list::set(int pos, Elem e, const List& l)        /* modifica l'elemento in posizione pos, se la posizione e' ammissibile */
 {
-   
+    if(pos > size(l)){
+        return;
+    }else{
+        int n = 0;
+        node* cur = l->next;
+        while((cur != l) && (n != pos)){
+            n++;
+            cur = cur->next;
+        }
+        cur->info = e;
+    }
 }
 
 
 void list::add(int pos, Elem e, const List& l)        /* inserisce l'elemento in posizione pos, shiftando a destra gli altri elementi */
-{                                               
-  
+{              
+    if(pos > size(l)){
+        return;
+    }if(pos == 0){
+        addFront(e, l);
+    }if(pos == size(l)){
+        addRear(e, l);
+    }else{
+        int n = 0;
+        node* aux = new node;
+        node* cur = l->next;
+        aux->info = e;
+        while((cur != l) && (n != pos)){
+            n++;
+            cur = cur->next;
+        }
+        aux->next = cur;
+        aux->prev = cur->prev->next;
+        cur->prev->next = aux;
+        cur->prev = aux;
+    }
 }
 
 
 void list::addRear(Elem e,  const List& l)        /* inserisce l'elemento alla fine della lista */
 {
-    
+    node* aux = new node;
+    aux->info = e;
+    if(isEmpty(l)){
+        aux->next = l;
+        aux->prev = l;
+        l->next = aux;
+        l->prev = aux;
+    }else{
+        aux->next = l;
+        aux->prev = l->prev;
+        l->prev->next = aux;
+        l->prev = aux;
+    }
 }
 
 
 void list::addFront(Elem e, const List& l)        /* inserisce l'elemento all'inizio della lista */
 {
-     
+    node* aux = new node;
+    aux->info = e;
+    if(isEmpty(l)){
+        aux->next = l;
+        aux->prev = l;
+        l->prev = aux;
+        l->next = aux;
+    }else{
+        aux->prev = l;
+        aux->next = l->next->next;
+        l->next->prev = aux;
+        l->next = aux;
+    }
 }
 
 
 void list::removePos(int pos, const List& l)      /* cancella l'elemento in posizione pos dalla lista */
 {
-
+    if(pos > size(l)){
+        return;
+    }else{
+        int n = 0;
+        node* cur = l->next;
+        while((cur != l) && (n != pos)){
+            n++;
+            cur = cur->next;
+        }
+        cur->prev->next = cur->next;
+        cur->next->prev = cur->prev;
+        delete cur;
+    }
 }
 
 
 void list::removeEl(Elem e, const List& l)        /* cancella tutte le occorrenze dell'elemento elem, se presenti, dalla lista */
 {
-
+    node *cur = l->next;
+    while(cur != l){
+        if(cur->info == e){
+            cur->prev->next = cur->next;
+            cur->next->prev = cur->prev;
+            delete cur;
+            return;
+        }else{
+            cur = cur->next;
+        }
+    }
 }
 
 
 bool list::isEmpty(const List& l)         /* restituisce true se la lista e' vuota (ed e' vuota se il next di l, e' l stessa */
 {
-  
+    return (l->next == l);
 }
 
 
 int list::size(const List& l)           /* restituisce la dimensione della lista */
 {
-   
+    int s = 0;
+    node * cur = l->next;
+    while(cur != l){
+        s++;
+        cur = cur->next;
+    }
+    return s;
 }
 
 
 void list::createEmpty(List& l)           /* crea la lista vuota */
 {
-   
+    l = new node;
+    l->next = l;
+    l->prev = l;
 }
